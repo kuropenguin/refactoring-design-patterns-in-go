7756a617540ae3e7782730e95a1f38eb2dbeeaea
kuropenguin <kuro1994.t@gmail.com>
2023-02-14 20:54:16 +0900

    add abstract-factory

---
+++ abstract-factory/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	adidasFactory, _ := GetSportsFactory("adidas")
+	nikeFactory, _ := GetSportsFactory("nike")
+
+	adidasShoe := adidasFactory.makeShoe()
+	adidasShirt := adidasFactory.makeShirt()
+
+	nikeShoe := nikeFactory.makeShoe()
+	nikeShirt := nikeFactory.makeShirt()
+
+	printShoeDetails(adidasShoe)
+	printShirtDetails(adidasShirt)
+
+	printShoeDetails(nikeShoe)
+	printShirtDetails(nikeShirt)
+}
+
+func printShoeDetails(s IShoe) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
+
+func printShirtDetails(s IShirt) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
